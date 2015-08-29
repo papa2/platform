642fb556e6de1e3193a44f8bc8f5189dd750159f
Jiakun Xu <jiakunxu@gmail.com>
2015-08-29 14:09:40 +0800

    y优化

--- biz/src/main/java/com/papa2/platform/monitor/dao/impl/CacheMonitorDaoImpl.java
+++ biz/src/main/java/com/papa2/platform/monitor/dao/impl/CacheMonitorDaoImpl.java
@@ -31,18 +31,18 @@ public class CacheMonitorDaoImpl extends BaseDaoImpl implements ICacheMonitorDao
 
 	@Override
 	public String createCacheMonitor(final List<CacheStats> cacheStatsList) {
-		return (String) getSqlMapClientTemplate().execute(new SqlMapClientCallback() {
-			StringBuilder sb = new StringBuilder();
-
-			public Object doInSqlMapClient(SqlMapExecutor executor) throws SQLException {
+		return getSqlMapClientTemplate().execute(new SqlMapClientCallback<String>() {
+			public String doInSqlMapClient(SqlMapExecutor executor) throws SQLException {
 				executor.startBatch();
 
+				StringBuilder sb = new StringBuilder();
 				for (CacheStats s : cacheStatsList) {
 					if (sb.length() != 0) {
 						sb.append(",");
 					}
 					sb.append(executor.insert("monitor.cache.createCacheMonitor", s));
 				}
+
 				executor.executeBatch();
 
 				return sb.toString();
